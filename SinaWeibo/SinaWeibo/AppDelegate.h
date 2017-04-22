0157ee924023d381c1a3c5c0bfb794da7a47e260
taolei <taolei@taoleideMac.local>
2017-04-22 09:51:27 +0800

    初始化项目

---
+++ SinaWeibo/SinaWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinaWeibo
+//
+//  Created by taolei on 17/4/22.
+//  Copyright © 2017年 wellgood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SinaWeibo/SinaWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SinaWeibo
+//
+//  Created by taolei on 17/4/22.
+//  Copyright © 2017年 wellgood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
