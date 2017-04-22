2b700c9d08fecebe9747c2495c1e290626684efd
taolei <taolei@taoleidemac.local>
2017-04-22 10:01:51 +0800

    1..

---
+++ SinaWeibo/SinaWeibo/MyMainTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MyMainTabBarController.h
+//  SinaWeibo
+//
+//  Created by taolei on 17/4/22.
+//  Copyright © 2017年 wellgood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyMainTabBarController : UITabBarController
+
+@end
